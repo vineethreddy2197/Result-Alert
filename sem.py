188d3496e0c079355b86687b58034c111e34d808
vineethreddy2197 <vineethreddy2197@gmail.com>
2018-02-12 21:02:48 +0530

    Env problem resolved

--- sem.py
+++ sem.py
@@ -6,14 +6,14 @@ while(True):
     page=requests.get('http://www.vce.ac.in/COE.aspx')
     data=page.text
     if data.count("01-2018")==1:
-        time.sleep(60)
+        time.sleep(120)
         print("No")
         continue
     else:
         flag=1
         break
 if flag==1:
-    os.system('python sms.py')
+    os.system('C:\\Users\\Vineeth\\AppData\\Local\\Programs\\Python\\Python35-32\\python sms.py')
     mixer.init()
     mixer.music.load('D:\Songs\English\Others\Summer.mp3')
     mixer.music.play()
